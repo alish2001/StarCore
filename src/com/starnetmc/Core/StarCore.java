30a942f37c48d12734b7bd658da3199efff6e365
Ali | alish2001 <ali.shariat2001@gmail.com>
2016-01-01 16:18:47 -0500

    Initial Commit
    The Start of Star Network-Wars Episode 4 | A New Hope

---
+++ src/com/starnetmc/Core/StarCore.java
@@ -0,0 +1,19 @@
+package com.starnetmc.Core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Ali on 1/1/2016 at 4:10 PM.
+ */
+public class StarCore extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
